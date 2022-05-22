86cf803401f56d341548537b46bc53a79abc9d71
sardinhas12 <patriciasardinharocha@gmail.com>
2022-05-22 19:12:51 +0000

    new feat

--- src/app.js
+++ src/app.js
@@ -4,8 +4,43 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
+};
+const newCard = () => {
+  let cardContent = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Q",
+    "K",
+    "A",
+    " "
+  ];
+  let cardSuit = ["♥", "♠", "♣", "♦"];
+  let randomNumber = Math.floor(Math.random() * 13);
+  let randomSuit = Math.floor(Math.random() * 4);
+
+  document.getElementById("cardDealer").innerHTML = cardContent[randomNumber];
+  document.getElementById("cardSign").innerHTML = cardSuit[randomSuit];
+  document.getElementById("cardSignBottom").innerHTML = cardSuit[randomSuit];
+
+  if (cardSuit[randomSuit] === "♥" || cardSuit[randomSuit] === "♦") {
+    var element = document.getElementById("cardSign");
+    element.classList.add("red");
+    var element2 = document.getElementById("cardSignBottom");
+    element2.classList.add("red");
+  } else if (cardSuit[randomSuit] === "♣" || cardSuit[randomSuit] === "♠") {
+    element = document.getElementById("cardSign");
+    element.classList.remove("red");
+    element2 = document.getElementById("cardSignBottom");
+    element2.classList.remove("red");
+  }
 };
