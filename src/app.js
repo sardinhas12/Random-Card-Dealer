1ac7040e45c51264fe2b362925364ba83a1279b4
sardinhas12 <patriciasardinharocha@gmail.com>
2022-08-03 17:26:47 +0000

    Align number and add button

--- src/app.js
+++ src/app.js
@@ -28,19 +28,24 @@ const newCard = () => {
   let randomNumber = Math.floor(Math.random() * 13);
   let randomSuit = Math.floor(Math.random() * 4);
 
-  document.getElementById("cardDealer").innerHTML = cardContent[randomNumber];
-  document.getElementById("cardSign").innerHTML = cardSuit[randomSuit];
+  document.getElementById("cardNumber").innerHTML = cardContent[randomNumber];
+  document.getElementById("cardSignTop").innerHTML = cardSuit[randomSuit];
   document.getElementById("cardSignBottom").innerHTML = cardSuit[randomSuit];
 
   if (cardSuit[randomSuit] === "♥" || cardSuit[randomSuit] === "♦") {
-    var element = document.getElementById("cardSign");
+    var element = document.getElementById("cardSignTop");
     element.classList.add("red");
     var element2 = document.getElementById("cardSignBottom");
     element2.classList.add("red");
   } else if (cardSuit[randomSuit] === "♣" || cardSuit[randomSuit] === "♠") {
-    element = document.getElementById("cardSign");
+    element = document.getElementById("cardSignTop");
     element.classList.remove("red");
     element2 = document.getElementById("cardSignBottom");
     element2.classList.remove("red");
   }
+};
+window.onload = function() {
+  newCard();
+  let buttonClick = document.getElementById("btn");
+  buttonClick.addEventListener("click", newCard);
 };
